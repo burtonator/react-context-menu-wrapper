9875c59de694b9b55339b15f8e2809ced47e53e2
v1ndic4te <vndc4t0r@gmail.com>
2018-12-02 09:08:38 +0000

    Update JSX source for demos

---
+++ NestedExample.jsx
@@ -0,0 +1,39 @@
+const React = require('react');
+import {ContextMenuWrapper, prepareContextMenuHandlers} from 'react-context-menu-wrapper';
+
+const boxes = [
+    ['Geralt', {backgroundColor: '#ffcccc', color: '#4c1422'}],
+    ['Yennefer', {backgroundColor: '#e2f2fd', color: '#0d3552'}],
+    ['Ciri', {backgroundColor: '#a7f2e7', color: '#00634f'}],
+];
+
+export class NestedExample extends React.Component {
+    renderBox(boxAcc) {
+        if (boxAcc.length === 0) return;
+
+        const box = boxAcc.shift();
+        const name = box[0];
+        const style = box[1];
+        const handlers = prepareContextMenuHandlers(name);
+        return <div className="my-box-nested" style={style} {...handlers}>
+            <span>{name}</span>
+            {this.renderBox(boxAcc)}
+
+            <ContextMenuWrapper id={name}>
+                <div className="dropdown is-active">
+                    <div className="dropdown-menu">
+                        <div className="dropdown-content" style={{backgroundColor: '#ffe5ea'}}>
+                            <div className="dropdown-item">
+                                This menu belongs to <strong>{name}</strong>!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </ContextMenuWrapper>
+        </div>;
+    }
+
+    render() {
+        return this.renderBox(boxes.slice(0));
+    }
+}
